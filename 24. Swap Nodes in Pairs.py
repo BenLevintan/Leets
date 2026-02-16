157e212187c0cfcee61056dd6a71a271b927a118
Ben <benlevintan@gmail.com>
2026-02-16 08:03:41 +0200

    added the start of question 24

---
+++ 24. Swap Nodes in Pairs.py
@@ -0,0 +1,18 @@
+# Need to create a dummy node or two
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
+
+sol = Solution.swapPairs()
+print
