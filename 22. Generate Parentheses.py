219304c65e24c7a04473490b6b1b271faecccc0b
Ben <benlevintan@gmail.com>
2026-02-16 07:53:32 +0200

    added solution to question number 22

--- 22. Generate Parentheses.py
+++ 22. Generate Parentheses.py
@@ -4,4 +4,24 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        
+        result_list = []
+
+        def backtrack(current_string, open, closed):
+            # Check if all parenthesis are used -> add to list
+            if len(current_string) >= 2 * n:
+                result_list.append(current_string)
+                return
+
+            if open < n:
+                backtrack(current_string + "(", open + 1, closed)
+
+            if closed < open:
+                backtrack(current_string + ")", open, closed + 1)              
+            
+
+        backtrack("", 0, 0)
+        return result_list
+
+
+sol = Solution()
+print(sol.generateParenthesis(2))
